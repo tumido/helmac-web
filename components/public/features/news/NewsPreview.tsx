b1a4b41a2eb489b6b03f2498382d2c0e466f4d02
Tomas Coufal <coufal.tom@gmail.com>
2026-05-03 19:58:13 +0200

    feat: change news cards to a feed
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- components/public/features/news/NewsPreview.tsx
+++ components/public/features/news/NewsPreview.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import { Box, Grid, Button, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { ArrowForward } from "@mui/icons-material";
 import { getLatestNewsForActiveYear } from "@/lib/services";
-import { NewsCard } from "./NewsCard";
+import { NewsFeed } from "./NewsFeed";
 
 export async function NewsPreview() {
     const news = await getLatestNewsForActiveYear(3);
@@ -19,13 +19,7 @@ export async function NewsPreview() {
 
     return (
         <>
-            <Grid container spacing={4}>
-                {news.map((item) => (
-                    <Grid item key={item.id} xs={12} sm={6} md={4}>
-                        <NewsCard news={item} />
-                    </Grid>
-                ))}
-            </Grid>
+            <NewsFeed news={news} />
             <Box sx={{ textAlign: "center", mt: 4 }}>
                 <Link href="/novinky" style={{ textDecoration: "none" }}>
                     <Button variant="outlined" endIcon={<ArrowForward />}>
