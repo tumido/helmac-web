28d60cead777edba78023486f489723b716b85b3
jirivrbka <vrbka.jirka@gmail.com>
2026-05-12 19:39:45 +0200

    Account nav

--- components/public/features/account/account-nav.tsx
+++ components/public/features/account/account-nav.tsx
@@ -7,9 +7,9 @@ import { Dashboard, Person, EventNote, Payment } from "@mui/icons-material";
 
 const navItems = [
     { label: "Přehled", href: "/ucet", icon: <Dashboard /> },
-    { label: "Profil", href: "/ucet/profil", icon: <Person /> },
     { label: "Registrace", href: "/ucet/registrace", icon: <EventNote /> },
     { label: "Platby", href: "/ucet/platby", icon: <Payment /> },
+    { label: "Profil", href: "/ucet/profil", icon: <Person /> },
 ];
 
 export function AccountNav() {
